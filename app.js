e6faa50b4765d3f93a3dfd4dc24380ecfaf20afd
roshinys <roshinchaya@gmail.com>
2022-11-14 14:25:22 +0530

    starting files

---
+++ app.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const errorController = require("./controllers/error");
+const mongoConnect = require("./util/database").mongoConnect;
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+//modells
+const User = require("./models/user");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findById("6370ab72bebce2bc8640033b")
+    .then((user) => {
+      req.user = new User(user.username, user.email, user.cart, user._id);
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoConnect(() => {
+  //console.log(client);
+  app.listen(3000, () => {
+    console.log("server started at port 3k");
+  });
+});
---
+++ util/database.js
@@ -0,0 +1,27 @@
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(process.env.MONGO_URL)
+    .then((client) => {
+      // console.log("Connected!");
+      _db = client.db();
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDB = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No db found";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDB = getDB;
